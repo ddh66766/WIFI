efb78fadf6967e165ac1b0bf4047271ffe71e215
ddh66766 <82809046+ddh66766@users.noreply.github.com>
2021-04-19 11:52:49 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+$VR_MIUI = array(
+    'VR_MIUI' => 'MIUI Global 12.03 Estable 12.0.3.0(QFGUXM)',
+    'V_A' => '10 QKQ1.190910.002',
+    'N_Path' => '2021-01-01',
+    'Modelo' => 'M1901F7G',
+    'Version_banda_base' => '660_GEN_PACK-1.338647.1.345992.1',
+    'Version_karnel' => '4.4.192-perf +',
+    'IP' => 'fe80::78ce:e7ff:fe8d:5b67',
+    'IMEI_1' => '8660420461060979',
+    'IMEI_1' => '8660420461060979',
+    'MAC' => 'e0:dc:ff:10:e4:10',
+    'RAM' => '4,00GB',
+    'CPU' => 'Octa-core  Max 2.20GHz',
+    'Modelo' => 'Redmi Note 7',
+);
