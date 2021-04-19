543d0b6e65ba287fe4e3550af26909d7e2ded108
ddh66766 <82809046+ddh66766@users.noreply.github.com>
2021-04-19 11:53:47 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+include '../info.php';
+include '/WIFI/data/vendor/config.php';
+include '/WIFI/data/test.php';
+include '/WIFI/data/.idea/connect.php'
+
+
+?>
+
+<?php
+
+$error_wifi = function(mysqli_fetch_assoc) {
+    echo "Error to wifi";
+}
+
+
+$class = $error_wifi;
+
+if ($class == $error_wifi) {
+    echo "Error to wifi";
+}
+
+while ($error_wifi_v == $class) {
+    echo "Error to wifi";
+}
+
+$error_wifi_first = while ($connect == treu) {
+    echo $error_wifi
+} else {
+    echo $class
+}
+\
