fece20e0f0bf572fa63aef0483c2b45b5cfc4749
ddh66766 <82809046+ddh66766@users.noreply.github.com>
2021-04-19 11:58:09 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+include '../info.php';
+include '../test.php';
+
+?>
+
+<?php
+
+$connect = mysqli_connect('wifi', 'MiFibra-9696', 'Lpeta32382008', 'MIUI Global 12.03
+                                                                                                         Estable
+                                                                                                         12.0.3.0(QfGEUXM)');
+
+if ($connect == true) {
+    echo "OK";
+}
+
+?>
+
